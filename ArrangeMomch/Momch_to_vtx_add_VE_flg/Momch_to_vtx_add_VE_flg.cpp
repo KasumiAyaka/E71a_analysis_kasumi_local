7d618e5fb90539f31698deec249c8c142bc82b11
KasumiAyaka <kasumi@flab.phys.nagoya-u.ac.jp>
2026-05-02 17:31:34 +0900

    I modified the code to output the pb value for muons instead of protons.

--- ArrangeMomch/Momch_to_vtx_add_VE_flg/Momch_to_vtx_add_VE_flg.cpp
+++ ArrangeMomch/Momch_to_vtx_add_VE_flg/Momch_to_vtx_add_VE_flg.cpp
@@ -235,7 +235,7 @@ void read_stop_txt(std::vector<Momentum_recon::Event_information>& momch, std::m
 				stop_tmp.mom = c.ecc_mcs_mom[1];
 				stop_tmp.rng = c.ecc_range_mom[1];
 			}
-			stop_tmp.pb = c.Get_proton_mcs_pb();
+			stop_tmp.pb = c.Get_muon_mcs_pb();
 
 			if (stop_tmp.pl1 <= stop_tmp.stoppl) {//fwd
 				//stop
